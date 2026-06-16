2913f519f9e12c961e20d543ba693abe7c81e5e8
Pari9678 <vanillap1711@gmail.com>
2026-06-16 18:57:34 +1000

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("My first game screen")
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+font = pygame.font.Font(None, 36)
+text = font.render("Hello Pygame!", True, BLACK)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(WHITE)
+    pygame.draw.rect(screen, BLUE, (270, 190, 100, 100))
+    screen.blit(text, (220, 100))
+    pygame.display.update()
+
+pygame.quit()
